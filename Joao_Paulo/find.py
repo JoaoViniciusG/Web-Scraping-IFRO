235722598d48da42e3e7a214f48e889234e11bba
João Vinícius <joaovigu07@gmail.com>
2024-02-02 22:40:57 -0400

    Correções .4

--- Joao_Paulo/find.py
+++ Joao_Paulo/find.py
@@ -3,20 +3,70 @@ def findVendaJP(service, options) -> list:
     from selenium.webdriver.common.by import By
     try: from Joao_Paulo.main import linksVendaJP
     except: from main import linksVendaJP
+    from time import sleep
 
-    links = linksVendaJP(service, options)
+    links = linksVendaJP(service)
 
     driver = webdriver.Chrome(options=options, service=service)
 
-from webdriver_manager.chrome import ChromeDriverManager
-from selenium.webdriver.chrome.service import Service
-from selenium.webdriver.chrome.options import Options
-from selenium import webdriver
+    #Área total (área da propriedade), área construída, dormitórios, suítes, banheiros, vagas garagem, bairro, endereço, valor
+    infos = [[],[],[],[],[],[],[],[],[]]
 
-service = Service(ChromeDriverManager().install())
-options = Options()
-options.add_argument('headless')
-options.add_argument('log-level=3')
-options.add_argument('--blink-settings=imagesEnabled=false')
+    infos_sub_primary = [["quartos", "suites", "banheiros", "garagens", "área propriedade", "área construida"],
+                         [2,3,4,5,0,1]]
+    
+    for link in links:
+        driver.get(link)
+        print(f"{links.index(link)+1}/{len(links)}", link)
+        sleep(1.5)
 
-findVendaJP(service, options)
+        #Cômodos:
+        div_master_rooms = driver.find_elements(By.CLASS_NAME, "MuiStack-root.css-u46kmv")[0]
+
+        for div_rooms in div_master_rooms.find_elements(By.TAG_NAME, "div"):
+            try: index = infos_sub_primary[1][infos_sub_primary[0].index(div_rooms.text.split(":")[0].lower().strip())]
+            except: continue
+            
+            room_text = int(div_rooms.text.split(":")[1].strip())
+
+            if room_text == 0: room_text = ""
+            infos[index].append(room_text)
+
+        #Áreas:
+        for p_area in driver.find_elements(By.CLASS_NAME,"MuiBox-root.css-15ea5xd")[0].find_elements(By.TAG_NAME, "p"):
+            try: index = infos_sub_primary[1][infos_sub_primary[0].index(p_area.text.split(":")[0].lower().strip())]
+            except: pass
+
+            area_text = p_area.text.split(":")[1].replace(".","").replace(",",".").replace("m²","").strip()
+
+            try: 
+                area_text = float(area_text)
+                if area_text == int(area_text): area_text = int(area_text)
+                if area_text == 0: area_text = ""
+            except: continue
+
+            infos[index].append(area_text)
+
+        #Bairro e endereço:
+        head_infos = driver.find_elements(By.CLASS_NAME, "MuiStack-root.css-fq5082")[0]
+
+        for p_address in head_infos.find_elements(By.CLASS_NAME, "MuiTypography-root.MuiTypography-body1.css-1a04ivi"):
+            if p_address.text.lower().find("vilhena") == -1: continue
+
+            infos[7].append(p_address.text)
+
+            for index_neigh, p_neigh in enumerate(p_address.text.lower().split(",")):
+                if p_neigh.find("vilhena - ro") != -1: infos[6].append(p_address.text.split(",")[index_neigh-1].strip())
+
+        #Valor
+        for value_h4 in driver.find_elements(By.CLASS_NAME, "MuiTypography-root.MuiTypography-h4.css-f4uu5s"):
+            if value_h4.text.find("R$") == -1: continue
+
+            infos[8].append(float(value_h4.text.replace(".","").replace(",",".").replace("m²","").replace("R$","").strip()))
+
+        #Adiciona None nos campos sem informação:
+        for info in infos:
+            if len(info) < links.index(link) + 1: info.append("None")
+
+    driver.quit()
+    return infos
--- Joao_Paulo/main.py
+++ Joao_Paulo/main.py
@@ -1,58 +1,41 @@
-def linksVendaJP(service, options) -> list:
+def linksVendaJP(service) -> list:
     from selenium import webdriver
     from selenium.webdriver.common.by import By
     from selenium.webdriver.support.ui import WebDriverWait
     from selenium.webdriver.support import expected_conditions as EC
     from selenium.webdriver.common.action_chains import ActionChains
+    from selenium.webdriver.chrome.options import Options
     from time import sleep
 
     links = []
 
+    options = Options()
+    options.add_argument('log-level=3')
+
     driver = webdriver.Chrome(options=options, service=service)
     driver.get('https://joaopaulocorretordeimoveis.com.br/imovel/index/')
+    driver.maximize_window()
 
     words_to_search = ["chácara", "sítio", "fazenda"]
 
     WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.XPATH, '/html/body/div[1]/div/main/div/div/div[2]/div[3]/div[1]/div/span[2]')))
+    driver.find_element(By.XPATH, "/html/body/div[1]/div/main/div/div/div[2]/div[2]/form/div/div[2]/div[6]/div/input").send_keys("Vilhena")
     sleep(1)
+    height = driver.execute_script("return document.body.scrollHeight")
+    for num in [-1, 1/4, 2/4, 3/4]:
+        ActionChains(driver).scroll_by_amount(0, int(height*num)).perform()
+        sleep(1)
 
-    current_ad = 0
-    print(int(driver.find_element(By.XPATH, "/html/body/div[1]/div/main/div/div/div[2]/div[3]/div[1]/div/span[2]").text.split(" ")[0]) - 1)
-    while current_ad < int(driver.find_element(By.XPATH, "/html/body/div[1]/div/main/div/div/div[2]/div[3]/div[1]/div/span[2]").text.split(" ")[0]) - 1:
+    for div_ad in driver.find_elements(By.CLASS_NAME, "MuiPaper-root.MuiPaper-elevation.MuiPaper-rounded.MuiPaper-elevation0.css-1kbp4ht"):
+        ad_title = div_ad.find_elements(By.CLASS_NAME, "MuiTypography-root.MuiTypography-body1.MuiListItemText-primary.css-1wilzx2")[0].text.lower()
         
-        div_ad = driver.find_elements(By.CLASS_NAME, "MuiPaper-root.MuiPaper-elevation.MuiPaper-rounded.MuiPaper-elevation0.css-1kbp4ht")[current_ad]
+        #Remove imóveis rurais:
         for word in words_to_search:
-            text_title_ad = div_ad.find_elements(By.CLASS_NAME, "MuiTypography-root.MuiTypography-body1.MuiListItemText-primary.css-1wilzx2")[0].text.lower()
-            if text_title_ad.find(word) != -1: current_ad += 1; continue
+            if ad_title.find(word) != -1: break
+        else:
+            try: ad_num = div_ad.find_elements(By.CLASS_NAME, "MuiBox-root.css-o2945x")[0].get_attribute("src").split("/")[4]
+            except: continue
+            links.append(f"https://joaopaulocorretordeimoveis.com.br/imovel/show/habitacao/{ad_num}/")
 
-        ActionChains(driver).move_to_element(div_ad).perform()
-        div_ad.click()
-        
-        sleep(1)
-        links.append(driver.current_url)
-        current_ad += 1
-        
-        driver.back()
-        sleep(1)
-
-    print(links)
-    print(len(links))
-
-        
-
-
-
-
-
-from webdriver_manager.chrome import ChromeDriverManager
-from selenium.webdriver.chrome.service import Service
-from selenium.webdriver.chrome.options import Options
-from selenium import webdriver
-
-service = Service(ChromeDriverManager().install())
-options = Options()
-#options.add_argument('headless')
-options.add_argument('log-level=3')
-#options.add_argument('--blink-settings=imagesEnabled=false')
-
-linksVendaJP(service, options)
+    driver.quit()
+    return links
